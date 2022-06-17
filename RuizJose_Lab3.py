9a9c65172aa811a3e384332e058178ceedad6310
Jose305 <jaruiz17@espe.edu.ec>
2022-06-17 09:53:29 -0500

    Version 2

--- RuizJose_Lab3.py
+++ RuizJose_Lab3.py
@@ -7,3 +7,19 @@
 '''
 #Importamos la libreria Queue
 from queue import Queue 
+
+class Grafo():
+    '''
+    Atributos:
+        m_numero_de_nodos: int
+        m_nodos: int
+        m_dirigido: bool
+        m_adyacencia_lista: dict
+    
+    Métodos:
+        __init__(self, numero_de_nodos, dirigido=True):
+        añadir_nodo(self, nodo1, nodo2, peso=1):
+        mostrar_lista_adyacencia(self):
+        def dfs(self, nodo_de_inicio, objetivo, camino = [], visitado = set()):
+    '''
+    
