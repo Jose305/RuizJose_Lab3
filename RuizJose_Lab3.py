f721bd14d48901b231c6bfb6f9f4de5d27cf75e5
Jose305 <jaruiz17@espe.edu.ec>
2022-06-17 09:48:29 -0500

    Version 1

---
+++ RuizJose_Lab3.py
@@ -0,0 +1,9 @@
+'''
+    Universidad de las Fuerzas Armadas ESPE Sede Santo Domingo de los Colorados
+    Ciencias de la Computación
+    Ingeniería en Tecnologías de la Información
+    Autor: José Ruiz
+    Materia: Inteligencia artificial
+'''
+#Importamos la libreria Queue
+from queue import Queue 
