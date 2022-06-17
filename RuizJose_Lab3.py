02fd52726408ee783fa7af9c7bee33621e36c8cb
Jose305 <jaruiz17@espe.edu.ec>
2022-06-17 10:00:21 -0500

    Version 3

--- RuizJose_Lab3.py
+++ RuizJose_Lab3.py
@@ -22,4 +22,21 @@ class Grafo():
         mostrar_lista_adyacencia(self):
         def dfs(self, nodo_de_inicio, objetivo, camino = [], visitado = set()):
     '''
-    
+    def __init__(self, numero_de_nodos, dirigido=True):
+
+        '''
+        Parametros:
+            numero_de_nodos: int
+            dirigido: bool
+        Retorna:
+            nada
+        '''
+        #Designando el numero de nodos a cada nodo recibido por parámetro
+        self.m_numero_de_nodos = numero_de_nodos 
+        #Mide el rango de nodos
+        self.m_nodos = range(self.m_numero_de_nodos) 
+        #Dirigido o No Dirigido
+        self.m_dirigido = dirigido
+        #asigna la creación de un diccionario de datos
+        #determina el espacio en donde se almacenarán los nodos
+        self.m_adyacencia_lista = {self: set() for self in self.m_nodos} 
